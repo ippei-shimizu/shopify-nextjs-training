ae354ebacfd13bbcc74be073c99863f22fd2320f
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-04-09 18:33:43 +0900

    headerを追加した

---
+++ src/data/headerNavLinks.jsx
@@ -0,0 +1,11 @@
+const headerNavLinks = [
+  { href: "/", title: "ホーム" },
+  { href: "about", title: "農園について" },
+  { href: "news", title: "お知らせ" },
+  { href: "fruit", title: "商品について" },
+  { href: "shop", title: "オンラインショップ" },
+  { href: "owner", title: "オーナー制ぶどう" },
+  { href: "contact", title: "お問い合わせ" },
+];
+
+export default headerNavLinks;
